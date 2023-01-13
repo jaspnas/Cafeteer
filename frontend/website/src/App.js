05dae16f149cffcfe58876415c0d50ae9b3d43f3
MirruK <mirek.kudinoff@abo.fi>
2023-01-13 19:57:26 +0200

    Initial Frontend Commit, nothing to see here.

---
+++ frontend/website/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+
+const App = () => {
+    return(
+        <div>
+            <SearchBar />
+            <DateField />
+            <Summary/>
+        </div>
+    )
+}
+
+export default App;
