20f871a51aa822c8912f3dbe4195fe137a88d4d6
Jesse Aspnäs <jesse@jaspnas.com>
2023-01-13 23:27:16 +0200

    add bson to common_types

--- backend/common/common_types.go
+++ backend/common/common_types.go
@@ -3,42 +3,42 @@ package common
 import "time"
 
 type locationMeta struct {
-	Country string `json:"country"`
-	City    string `json:"city"`
+	Country string `json:"country" bson:"country"`
+	City    string `json:"city" bson:"city"`
 }
 
 type location struct {
-	Lat      float32      `json:"lat"`
-	Lon      float32      `json:"lon"`
-	Accuracy int          `json:"accuracy"`
-	Meta     locationMeta `json:"meta"`
+	Lat      float32      `json:"lat" bson:"lat"`
+	Lon      float32      `json:"lon" bson:"lon"`
+	Accuracy int          `json:"accuracy" bson:"accuracy"`
+	Meta     locationMeta `json:"meta" bson:"meta"`
 }
 
 type openingHours struct {
-	Open  time.Time `json:"open"`
-	Close time.Time `json:"close"`
+	Open  time.Time `json:"open" bson:"open"`
+	Close time.Time `json:"close" bson:"close"`
 }
 
 type RestaurantMetadata struct {
-	Name         string         `json:"name"`
-	Location     location       `json:"location"`
-	Chain        string         `json:"chain"`
-	OpeningHours []openingHours `json:"opening_hours"`
+	Name         string         `json:"name" bson:"name"`
+	Location     location       `json:"location" bson:"location"`
+	Chain        string         `json:"chain" bson:"chain"`
+	OpeningHours []openingHours `json:"opening_hours" bson:"opening_hours"`
 }
 
 type mealOption struct {
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	Diets       string  `json:"diets"`
-	Price       float32 `json:"price"`
+	Name        string  `json:"name" bson:"name"`
+	Description string  `json:"description" bson:"description"`
+	Diets       string  `json:"diets" bson:"diets"`
+	Price       float32 `json:"price" bson:"price"`
 }
 
 type dayMenu struct {
-	Date    time.Time    `json:"date"`
-	Options []mealOption `json:"options"`
+	Date    time.Time    `json:"date" bson:"date"`
+	Options []mealOption `json:"options" bson:"options"`
 }
 
 type Restaurant struct {
-	Meta RestaurantMetadata `json:"meta"`
-	Menu []dayMenu
+	Meta RestaurantMetadata `json:"meta" bson:"meta"`
+	Menu []dayMenu          `json:"menu" bson:"menu"`
 }
